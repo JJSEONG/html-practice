304fab95d715e78a58c82a7a6814fb1190e06ba9
JongSeong Jeong <hans3238@naver.com>
2022-01-25 22:44:42 +0900

    portfolio

--- portfolio-practice/js/click.js
+++ portfolio-practice/js/click.js
@@ -2,4 +2,10 @@ $(function(){
     $('.important').click(function(){
         $(this).css({color:'#fffacd'});
     });
+    $('.menu-btn a').mouseover(function(){
+        $(this).css({color:'#fffacd', textDecoration:'underline', textDecorationPosition:'under'});
+    }).mouseout(function(){
+        $(this).css({color:'#fff', textDecoration:'none'});
+    });
+    //------------------------
 });
